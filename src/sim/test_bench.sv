b14bd71b525673c3dc022c2ddc1c771455f1426b
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-16 00:17:49 -0600

    shggh

--- src/sim/test_bench.sv
+++ src/sim/test_bench.sv
@@ -19,13 +19,13 @@ module tb;
   always #10 clk =~ clk;
   des_if _if(clk);
   // conexion de la interface con el DUT
-  FP_Multiplier u0 (   .clk(_if.clk),
-               .r_mode(_if.r_mode),
-               .fp_X(_if.fp_X),
-               .fp_Y(_if.fp_Y),
-               .fp_Z(_if.fp_Z),
-               .ovrf(_if.ovrf),
-               .udrf(_if.udrf));
+  FP_Multiplier u0 (.clk(_if.clk),
+                      .r_mode(_if.r_mode),  
+                      .fp_X(_if.fp_X),
+                      .fp_Y(_if.fp_Y),
+                      .fp_Z(_if.fp_Z),
+                      .ovrf(_if.ovrf),
+                      .udrf(_if.udrf));
   initial begin
     clk <= 0;
     uvm_config_db#(virtual des_if)::set(null,"uvm_test_top","des_vif",_if);
