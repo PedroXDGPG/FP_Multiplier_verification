ed3aa620f8a9a986783628a7cbcb7505248d6577
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-16 00:21:50 -0600

    estupidez

--- src/sim/test_bench.sv
+++ src/sim/test_bench.sv
@@ -19,7 +19,7 @@ module tb;
   always #10 clk =~ clk;
   des_if _if(clk);
   // conexion de la interface con el DUT
-  FP_Multiplier u0 (.clk(_if.clk),
+  top u0 (.clk(_if.clk),
                       .r_mode(_if.r_mode),  
                       .fp_X(_if.fp_X),
                       .fp_Y(_if.fp_Y),
