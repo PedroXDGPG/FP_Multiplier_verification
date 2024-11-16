88d5c27506b27875f59b0434e2ede20ebf2014f1
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-16 17:09:11 -0600

    prueba

--- src/sim/scoreboard.sv
+++ src/sim/scoreboard.sv
@@ -20,8 +20,8 @@ class scoreboard extends uvm_scoreboard;
     // Componentes del número flotante
     bit sign_X, sign_Y, sign_Z;
     bit [7:0] exp_X, exp_Y, exp_Z;
-    bit [23:0] sig_X, sig_Y;
-    bit [47:0] sig_Z;             // 48 bits para evitar desbordamiento
+    bit [23:0] man_X, man_Y;      // 24 bits para evitar desbordamiento
+    bit [47:0] man_Z;             // 48 bits para evitar desbordamiento
     bit [47:0] product;           
     int i;
 
@@ -30,13 +30,13 @@ class scoreboard extends uvm_scoreboard;
     sign_Y = item.fp_Y[31];
     exp_X = item.fp_X[30:23];
     exp_Y = item.fp_Y[30:23];
-    sig_X = {1'b1, item.fp_X[22:0]};
-    sig_Y = {1'b1, item.fp_Y[22:0]};
+    man_X = {1'b1, item.fp_X[22:0]};
+    man_Y = {1'b1, item.fp_Y[22:0]};
 
-    // Verificar casos especiales
+    ////////////////////////////////// Verificar casos especiales //////////////////////////////////
     if ((exp_X == 8'hFF && item.fp_X[22:0] != 0) || (exp_Y == 8'hFF && item.fp_Y[22:0] != 0)) begin
       // Caso de NaN
-      expected_fp_Z = {sign_X ^ sign_Y, 8'hFF, 23'h400000}; // NaN
+      expected_fp_Z = {sign_X ^ sign_Y, 8'hFF, 23'h800000}; // NaN
 
     end 
     
@@ -45,7 +45,7 @@ class scoreboard extends uvm_scoreboard;
       if ((exp_X == 8'hFF && item.fp_X[22:0] == 0 && item.fp_Y == 0) || 
           (exp_Y == 8'hFF && item.fp_Y[22:0] == 0 && item.fp_X == 0)) begin
         // Caso de cero * infinito
-        expected_fp_Z = {sign_X ^ sign_Y, 8'hFF, 23'h400000}; // NaN
+        expected_fp_Z = {sign_X ^ sign_Y, 8'hFF, 23'h800000}; // NaN
       end 
       
       else begin
@@ -59,40 +59,40 @@ class scoreboard extends uvm_scoreboard;
       expected_fp_Z = {sign_X ^ sign_Y, 8'h00, 23'h000000}; // Cero
 
     end 
-    
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
     else begin
       // Realizar la multiplicación punto flotante bit a bit
       product = 0;
       for (i = 0; i < 24; i++) begin
-        if (sig_Y[i]) begin
-          product = product + (sig_X << i);
+        if (man_Y[i]) begin
+          product = product + (man_X << i);
         end
       end
-      sig_Z = product;
+      man_Z = product;
 
       // Ajustar el exponente
       exp_Z = exp_X + exp_Y - 8'd127;
       sign_Z = sign_X ^ sign_Y;
 
       // Normalizar el resultado
-      if (sig_Z[47]) begin
-        sig_Z = sig_Z >> 1;
+      if (man_Z[47]) begin
+        man_Z = man_Z >> 1;
         exp_Z = exp_Z + 1;
       end 
       
       else begin
-        while (sig_Z[46] == 0 && exp_Z > 0) begin
-          sig_Z = sig_Z << 1;
+        while (man_Z[46] == 0 && exp_Z > 0) begin
+          man_Z = man_Z << 1;
           exp_Z = exp_Z - 1;
         end
       end
 
-      // Aplicar modo de redondeo
+      ///////////////////////////////// Aplicar modo de redondeo   /////////////////////////////////////
       case (item.r_mode)
         3'b000: begin
           // Redondeo al más cercano, con empates hacia par
-          if (sig_Z[23:0] > 24'h800000 || (sig_Z[23:0] == 24'h800000 && sig_Z[24])) begin
-            sig_Z = sig_Z + 1;
+          if (man_Z[23:0] > 24'h800000 || (man_Z[23:0] == 24'h800000 && man_Z[24])) begin
+            man_Z = man_Z + 1;
           end
         end
 
@@ -103,22 +103,22 @@ class scoreboard extends uvm_scoreboard;
 
         3'b010: begin
           // Redondeo hacia abajo (hacia -infinito)
-          if (sign_Z && sig_Z[23:0] != 0) begin
-            sig_Z = sig_Z + 1;
+          if (sign_Z && man_Z[23:0] != 0) begin
+            man_Z = man_Z + 1;
           end
         end
 
         3'b011: begin
           // Redondeo hacia arriba (hacia +infinito)
-          if (!sign_Z && sig_Z[23:0] != 0) begin
-            sig_Z = sig_Z + 1;
+          if (!sign_Z && man_Z[23:0] != 0) begin
+            man_Z = man_Z + 1;
           end
         end
 
         3'b100: begin
           // Redondeo al más cercano, empates hacia la magnitud máxima
-          if (sig_Z[23:0] > 24'h800000 || (sig_Z[23:0] == 24'h800000 && !sign_Z)) begin
-            sig_Z = sig_Z + 1;
+          if (man_Z[23:0] > 24'h800000 || (man_Z[23:0] == 24'h800000 && !sign_Z)) begin
+            man_Z = man_Z + 1;
           end
         end
 
@@ -141,7 +141,7 @@ class scoreboard extends uvm_scoreboard;
       end 
       
       else begin
-        expected_fp_Z = {sign_Z, exp_Z[7:0], sig_Z[46:24]};
+        expected_fp_Z = {sign_Z, exp_Z[7:0], man_Z[46:24]};
         expected_ovrf = 0;
         expected_udrf = 0;
       end
