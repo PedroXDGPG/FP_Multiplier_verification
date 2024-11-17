13e5e5252c8daa422a3680a98c46591ec5329e68
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-16 23:32:58 -0600

    while

--- src/sim/scoreboard.sv
+++ src/sim/scoreboard.sv
@@ -80,9 +80,9 @@ class scoreboard extends uvm_scoreboard;
       sticky_bit  = |man_Z[20:0];  
 
       // Normalizar el resultado
-      if (man_Z[47]) begin
+      while (man_Z[47]) begin
         man_Z = man_Z >> 1;
-        //exp_Z = exp_Z + 1;
+        exp_Z = exp_Z + 1;
       end 
       
       // else begin
