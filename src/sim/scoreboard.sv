535894fdcc060f46bb1c59cd7d14efa2d763de6c
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-17 17:15:38 -0600

    ascdscdas

--- src/sim/scoreboard.sv
+++ src/sim/scoreboard.sv
@@ -72,7 +72,7 @@ class scoreboard extends uvm_scoreboard;
 
       // Ajustar el exponente
       exp_Z = exp_X + exp_Y - 127;
-
+      exp_Z_num = exp_X + exp_Y;
       // Mostrar exponentes de X y Y
       `uvm_info("SCBD", $sformatf("EXPONENTE RESULTADO: exp_Z=%0h ", exp_Z), UVM_LOW)
  
@@ -138,7 +138,7 @@ class scoreboard extends uvm_scoreboard;
       endcase
 
       ///////////////////////////////// Manejar overflow y underflow /////////////////////////////////
-      if (exp_Z >= 8'hFF) begin
+      if (exp_Z_num >= 8'hFF) begin
         expected_fp_Z = {sign_Z, 8'hFF, 23'h000000}; // Infinito
         expected_ovrf = 1;
         expected_udrf = 0;
