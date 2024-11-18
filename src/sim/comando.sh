c696d8e5de377a0126b2c34008f58a0c40ba8637
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-18 17:54:02 -0600

    fvds

--- src/sim/comando.sh
+++ src/sim/comando.sh
@@ -3,12 +3,12 @@ rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
 
 vcs -Mupdate test_bench.sv  -o salida -full64 -debug_all -sverilog -kdb -l log_test -ntb_opts uvm-1.2 +lint=TFIPC-L -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH;
 
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=FP_Multiplier +ntb_random_seed=1 > deleteme_log_1;
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_000 +ntb_random_seed=1 > deleteme_log_000;
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_001 +ntb_random_seed=2 > deleteme_log_001;
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_010 +ntb_random_seed=3 > deleteme_log_010;
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_011 +ntb_random_seed=4 > deleteme_log_011;
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_100 +ntb_random_seed=5 > deleteme_log_100;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=FP_Multiplier +ntb_random_seed=1 > deleteme_log_1;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_000 +ntb_random_seed=1 > deleteme_log_000;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_001 +ntb_random_seed=2 > deleteme_log_001;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_010 +ntb_random_seed=3 > deleteme_log_010;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_011 +ntb_random_seed=4 > deleteme_log_011;
+./salida -cm line+tgl+assert +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test_FP_Multiplier_rmode_100 +ntb_random_seed=5 > deleteme_log_100;
 verdi -cov -covdir salida.vdb & -gui;
 
 #./salida -cm line+tgl+cond+fsm+branch+assert;
