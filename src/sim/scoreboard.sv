acc92e9b314c5a0dd4d6c61a5007fb03c5700b00
pedroxdgpg <pedro.medinila@estudiantec.cr>
2024-11-17 18:51:43 -0600

    asdfasdfsda

--- src/sim/scoreboard.sv
+++ src/sim/scoreboard.sv
@@ -19,8 +19,8 @@ class scoreboard extends uvm_scoreboard;
 
     // Componentes del número flotante
     bit sign_X, sign_Y, sign_Z;
-    bit [7:0] exp_X, exp_Y, exp_Z, exp_Z_no_sum;
-    bit [8:0] exp_Z_num;
+    bit [7:0] exp_X, exp_Y, exp_Z;
+    bit [8:0] exp_Z_num, exp_Z_no_sum;
     bit [23:0] man_X, man_Y;
     bit [47:0] man_Z;
     bit        round_bit, guard_bit, sticky_bit;
